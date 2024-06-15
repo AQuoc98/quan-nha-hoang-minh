40fc0be747809d59d69b5570d8abc0f88fe57290
AQuoc98 <badguykendf@gmail.com>
2024-06-16 02:04:17 +0700

    feat: setup project

---
+++ apps/store/src/app/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from '@org/shared';
+
+export default function Index() {
+  /*
+   * Replace the elements below with your own.
+   *
+   * Note: The corresponding styles are in the ./index.tailwind file.
+   */
+  return (
+    <div>
+      <Button />
+    </div>
+  );
+}
